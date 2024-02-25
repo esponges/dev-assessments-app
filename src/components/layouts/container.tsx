dde2db9d8f57b2f74c8b07f4d1520c6eebd65ee9
Fernando González Tostado <68973307+esponges@users.noreply.github.com>
2024-02-25 10:18:37 -0600

    show table with mock data

--- src/components/layouts/container.tsx
+++ src/components/layouts/container.tsx
@@ -1,6 +1,19 @@
-export function Container({ children }: { children: React.ReactNode }) {
+import { cn } from '@/lib/utils';
+
+export function Container({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) {
   return (
-    <main className='flex min-h-screen flex-col items-center py-12 md:w-1/4 w-3/4 mx-auto'>
+    <main
+      className={cn(
+        'flex min-h-screen flex-col items-center py-12 md:w-1/4 w-3/4 mx-auto',
+        className
+      )}
+    >
       {children}
     </main>
   );
