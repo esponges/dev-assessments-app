2c193ae5d95701ea364cc49e29a35f959f75f760
Fernando González Tostado <68973307+esponges@users.noreply.github.com>
2024-02-28 19:01:32 -0600

    submit answers

--- src/pages/app/evaluate/index.tsx
+++ src/pages/app/evaluate/index.tsx
@@ -27,12 +27,69 @@ type Assessment = {
     question_topic: string;
     choices?: string[];
     correct_answer: string;
+    selected_answer?: string;
   }[];
 };
 
 type MutationVariables = {
   stack: TechStack;
 };
+
+const testAssessment = [
+  {
+    question_text:
+      'What is a key difference between class and functional components in React?',
+    question_type: 'MULTIPLE_CHOICE',
+    question_topic: 'React',
+    choices: [
+      "Class components have state, functional components don't",
+      "Functional components have state, class components don't",
+      'Class components are written in JavaScript, functional components in TypeScript',
+      "Functional components use render(), class components don't",
+    ],
+    correct_answer: "Class components have state, functional components don't",
+  },
+  {
+    question_text: 'What does redux help you manage in your application?',
+    question_type: 'MULTIPLE_CHOICE',
+    question_topic: 'Redux',
+    choices: [
+      'Server requests',
+      'Database queries',
+      'Application state',
+      'UI Library installation',
+    ],
+    correct_answer: 'Application state',
+  },
+  {
+    question_text: 'What problem does TypeScript solve?',
+    question_type: 'FREE_RESPONSE',
+    question_topic: 'TypeScript',
+    choices: [],
+    correct_answer:
+      'TypeScript adds static types to JavaScript, helping to catch errors early in the development process',
+  },
+  {
+    question_text: 'What is a snapshot test in Jest?',
+    question_type: 'MULTIPLE_CHOICE',
+    question_topic: 'Jest',
+    choices: [
+      'A test that checks if the application state matches a snapshot in time',
+      'A test that checks if the UI matches a stored snapshot',
+      'A test that checks if the UI loads within a specified time',
+      'A test that takes a literal snapshot of the UI state',
+    ],
+    correct_answer: 'A test that checks if the UI matches a stored snapshot',
+  },
+  {
+    question_text: 'What is the primary function of GraphQL?',
+    question_type: 'FREE_RESPONSE',
+    question_topic: 'GraphQL',
+    choices: [],
+    correct_answer: `GraphQL is a specification for how to request and return data from servers to clients. 
+      It provides a more efficient, powerful and flexible alternative to REST`,
+  },
+];
 
 const getDevDetails = async (id: string) => {
   const res = await fetch(
@@ -59,7 +116,7 @@ const generateAssessment = async ({ stack }: MutationVariables) => {
       body: JSON.stringify({
         stack,
         promptOpt: ASSESSMENT_PROMPT_TYPE,
-        number_of_questions: 10, // hard coded for now
+        number_of_questions: 2, // hard coded for now
       }),
       headers: {
         'Content-Type': 'application/json',
@@ -79,7 +136,10 @@ const generateAssessment = async ({ stack }: MutationVariables) => {
 export default function Evaluate() {
   const router = useRouter();
   const { id } = router.query;
-  const [assessment, setAssessment] = useState<Assessment | null>();
+  const [assessment, setAssessment] = useState<Assessment | null>({
+    title: 'some assessment',
+    questions: testAssessment,
+  });
   const [techStack, setTechStack] = useState<TechStack>([]);
 
   // todo: this might be done elsewhere, probably when the page is loaded and the
@@ -120,6 +180,36 @@ export default function Evaluate() {
     }
   };
 
+  const handleOptionChange = (
+    chosenOption: number | string,
+    questionIdx: number
+  ) => {
+    setAssessment((prev) => {
+      if (prev) {
+        const newQuestions = [...prev.questions];
+
+        if (typeof chosenOption === 'number') {
+          newQuestions[questionIdx].selected_answer =
+            newQuestions[questionIdx].choices?.[chosenOption];
+        } else {
+          newQuestions[questionIdx].selected_answer = chosenOption;
+        }
+
+        return {
+          ...prev,
+          questions: newQuestions,
+        };
+      }
+
+      return prev;
+    });
+  };
+
+  // use the form radio group to handle the selected answer
+  const handleAssessmentSubmit = () => {
+    console.log(assessment);
+  };
+
   return (
     <Container className="px-6">
       {!assessment ? (
@@ -144,37 +234,47 @@ export default function Evaluate() {
           </Button>
         </>
       ) : (
-        assessment.questions.map((question, index) => (
-          // todo: abstract this into a component?
-          <form
-            key={index}
-            className="my-4 md:w-3/4 w-full border border-gray-300 p-4 pt-0 rounded-lg"
+        <form className="flex flex-col items-center space-y-8">
+          {assessment.questions.map((question, index) => (
+            // todo: abstract this into a component?
+            <div
+              key={index}
+              className="my-4 md:w-3/4 w-full border border-gray-300 p-4 pt-0 rounded-lg"
+            >
+              <Heading variant="h2">{question.question_text}</Heading>
+              {question.question_type === 'MULTIPLE_CHOICE' ? (
+                <RadioGroup>
+                  {question.choices?.map((choice, i) => (
+                    <div
+                      key={i}
+                      className="flex items-center space-x-2"
+                      onChange={() => handleOptionChange(i, index)}
+                    >
+                      <RadioGroupItem
+                        id={choice}
+                        value={choice}
+                      />
+                      <Label htmlFor={choice}>{choice}</Label>
+                    </div>
+                  ))}
+                </RadioGroup>
+              ) : (
+                // todo: use embedded editor instead
+                <textarea
+                  onChange={(e) => handleOptionChange(e.target.value, index)}
+                  className="w-full h-24 p-2 border border-gray-300 rounded-lg"
+                  placeholder="Type your answer here"
+                />
+              )}
+            </div>
+          ))}
+          <Button
+            className="mt-4"
+            onClick={handleAssessmentSubmit}
           >
-            <Heading variant="h2">{question.question_text}</Heading>
-            {question.question_type === 'MULTIPLE_CHOICE' ? (
-              <RadioGroup>
-                {question.choices?.map((choice, i) => (
-                  <div
-                    key={i}
-                    className="flex items-center space-x-2"
-                  >
-                    <RadioGroupItem
-                      id={choice}
-                      value={choice}
-                    />
-                    <Label htmlFor={choice}>{choice}</Label>
-                  </div>
-                ))}
-              </RadioGroup>
-            ) : (
-              // todo: use embedded editor instead
-              <textarea
-                className="w-full h-24 p-2 border border-gray-300 rounded-lg"
-                placeholder="Type your answer here"
-              />
-            )}
-          </form>
-        ))
+            Submit Assessment
+          </Button>
+        </form>
       )}
     </Container>
   );
