15021fd64d68ca623967d4ca6f82c95972535740
Fernando González Tostado <68973307+esponges@users.noreply.github.com>
2024-03-05 20:35:11 -0600

    reach out backend

--- src/pages/app/challenge/index.tsx
+++ src/pages/app/challenge/index.tsx
@@ -10,16 +10,48 @@ import { Button } from '@/components/ui/button';
 import type { Tech, TechStack } from '@/types';
 import { Editor } from '@/components/organisms/editor';
 
-type MutationResponse = {
+type CreateMutationResponse = {
   challenge: string;
 };
 
 const createChallenge = async (tech: Tech) => {
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/api/assessment/challenge/create`,
+    `${process.env.NEXT_PUBLIC_API_URL}/api/assessments/challenge/create`,
     {
       method: 'POST',
       body: JSON.stringify(tech),
+    }
+  );
+
+  if (!res.ok) {
+    throw new Error('Network response was not ok');
+  }
+
+  const json = await res.json();
+
+  return json;
+};
+
+type EvaluateMutationResponse = {
+  code_quality: number;
+  code_correctness: number;
+  code_efficiency: number;
+  code_maintainability: number;
+  feedback_message: string;
+};
+
+const evaluateChallenge = async ({
+  devResponse,
+  challenge,
+}: {
+  devResponse: string;
+  challenge: string;
+}) => {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/api/assessments/challenge/evaluate`,
+    {
+      method: 'POST',
+      body: JSON.stringify({ devResponse, challenge }),
     }
   );
 
@@ -53,10 +85,25 @@ export default function Challenge() {
   const [techStack, setTechStack] = useState<TechStack>(MOCKED_TECH_STACK);
   const [challenge, setChallenge] = useState<string>(MOCKED_CHALLENGE);
 
-  const { mutate, isPending } = useMutation<MutationResponse, Error, Tech>({
+  const { mutate, isPending } = useMutation<
+    CreateMutationResponse,
+    Error,
+    Tech
+  >({
     mutationFn: createChallenge,
     onSuccess: (data) => {
       setChallenge(data.challenge);
+    },
+  });
+
+  const { mutate: evaluate, isPending: isEvaluating } = useMutation<
+    EvaluateMutationResponse,
+    Error,
+    { devResponse: string; challenge: string }
+  >({
+    mutationFn: evaluateChallenge,
+    onSuccess: (data) => {
+      console.log(data);
     },
   });
 
@@ -69,13 +116,17 @@ export default function Challenge() {
     }
   };
 
+  const handleEvaluateChallenge = async (code: string) => {
+    evaluate({ devResponse: code, challenge });
+  };
+
   return (
     <Container>
       <Heading variant="h1">Code Challenge</Heading>
       {!challenge ? (
         <>
           <Alert
-            className="w-[20rem] my-4"
+            className="w-[20rem] my-4 disable-highlight"
             title="Please choose one technology from your tech stack"
             description="You will be given a code challenge based on the technology you choose"
           />
@@ -93,7 +144,7 @@ export default function Challenge() {
           </Button>
         </>
       ) : (
-        // todo: use a component that accepts markdown
+        // todo: use a component that accepts markdown instead of the Alert component
         <>
           <Alert
             title="Challenge"
@@ -103,7 +154,8 @@ export default function Challenge() {
           <Editor
             value=""
             language="javascript"
-            onContentSave={(val: string) => console.log(val)}
+            isLoading={isEvaluating}
+            onSubmit={handleEvaluateChallenge}
           />
         </>
       )}
