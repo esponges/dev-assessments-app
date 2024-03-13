b328ef93e595b3f2075d34bd1c4ca772f91a9ad0
Fernando González Tostado <68973307+esponges@users.noreply.github.com>
2024-03-12 19:03:44 -0600

    fetch dev details in navbar

--- src/components/layouts/pages/navigation.tsx
+++ src/components/layouts/pages/navigation.tsx
@@ -26,6 +26,7 @@ import {
   useUser,
   SignOutButton,
 } from '@clerk/nextjs';
+import { useQuery } from '@tanstack/react-query';
 
 const links = [
   { href: '/assess-profile', title: 'Assess Dev Profile' },
@@ -37,8 +38,29 @@ const links = [
   { href: '/devs/search', title: 'Get Developer Profiles' },
 ];
 
+const getDevDetails = async (id: string) => {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/api/candidate/details?id=${id}`
+  );
+
+  if (!res.ok) {
+    throw new Error('Network response was not ok');
+  }
+
+  const json = await res.json();
+
+  return json;
+};
+
 function AuthHeader() {
   const { user } = useUser();
+
+  // make user info available to the rest of the app
+  useQuery({
+    queryKey: ['user', user?.id],
+    queryFn: () => getDevDetails(user?.id || ''),
+    enabled: !!user?.id,
+  });
 
   return (
     <header className="flex items-center justify-end space-x-4">
@@ -57,7 +79,12 @@ function AuthHeader() {
               </SignedIn>
             </DropdownMenuTrigger>
             <DropdownMenuContent>
-              <DropdownMenuLabel>My Account</DropdownMenuLabel>
+              <Link
+                href="/account"
+                passHref
+              >
+                <DropdownMenuLabel>My Account</DropdownMenuLabel>
+              </Link>
               <DropdownMenuSeparator />
               <DropdownMenuItem>
                 <SignOutButton />
